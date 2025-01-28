05a3ebe16f80a54eeafa5d49f788d5bea30fa6ba
Jaque <jaquelinecosta-rj@hotmail.com>
2025-01-28 14:43:01 -0300

    finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './pages/viagens/Viagens'
 import Viagens from './pages/viagens/Viagens'
-import Navbar from './components/navbar/Navbar'
+import Navbar from './components/navbar/NavBar'
 import './App.css'
 function App() {
 
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -2,7 +2,7 @@
 function Navbar() {
     return (
         <>
-            <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
+            <div className='w-full flex justify-center py-4 bg-zinc-950 text-white'>
                 <div className="container flex justify-between text-lg">
                     <span>Carona Compartilhada</span>
                     <div className='flex gap-4'>
