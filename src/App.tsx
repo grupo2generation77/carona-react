4fa87a87c9d11948bad56b609f99191b51ecdd95
Ivan Barbosa <ivanjulio.ij@gmail.com>
2025-01-28 14:43:14 -0300

    "Ajustes de merge"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './pages/viagens/Viagens'
 import Viagens from './pages/viagens/Viagens'
 import './App.css'
+import About_us from './pages/about_us/About_us'
+
 function App() {
 
   return (
     <>
-      
+          <ToastContainer />
+            <BrowserRouter>
+                <Navbar />
+                <div className="min-h-[80vh]">
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/about_us" element={<About_us />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
     </>
   )
 }
